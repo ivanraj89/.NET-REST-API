e3ee6475cfe7085bee1d671012d2e4f6164aa122
ivanraj89 <ivanraj89@gmail.com>
2023-03-22 23:59:19 +0800

    MongoDb and docker

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.Repositories;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+
 
 var app = builder.Build();
 
